9886dea28a17f1acedf82f7784a4b5875e4a01dc
ripbog <crhinesmith5@gmail.com>
2022-08-01 21:37:39 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 04-createquiz
+
+This weeks challenge was to create a quiz application. The motivating factors behind this project was to get a better grasp on Javascript and Web APIs. We built the application with the specifications in mind in order to combine our knowledge of timers, local Storage, and Javascript logic into one project. I struggled with building in a way to display whether the answer selected was correct or not, so I used a method that would have made the question not change if the answer was wrong. I figured this would keep the spirit of the game intact because the user would still lose time to an incorrect answer and the score was a combination of points for the correct answer and time left at the end of the quiz. However, after reading the user story, I realized that this was not the intended result of the quiz, so I changed it at the end to where no matter what, if an answer button is selected, the next question appears. 
+
+[Screenshot 1 of the project](assets/finalwork-screenshot1)
+[Screenshot 2 of the project](assets/finalwork-screenshot2)
+
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var startButton = document.getElementById('start-btn');
-var nextButton = document.getElementById('next-btn');
+var cardEl = document.querySelector('.card')
 var questionContainer= document.getElementById('question-container');
 var questionEl = document.getElementById('question');
 var answersEl = document.getElementById('answers');
@@ -10,83 +10,12 @@ var secondsLeft = 90;
 var finalScore;
 var highScore = document.querySelector('.highscore');
 var backButton = document.getElementById('goBack-btn');
-
-
-startButton.addEventListener('click', start)
-
-function setTime(){
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = "Time Left: " + secondsLeft + " s ";
-
-        if(secondsLeft <= 0) {
-            clearInterval(timerInterval);
-            endGame();
-        }
-
-    }, 1000);
-}
-
-
-
-function start() {
-    startButton.classList.add('hide')
-    questionContainer.classList.remove('hide')
-    setTime()
-    nextQuestion(currentQuestion)
-}
-
-function nextQuestion(index) {
-    showQuestion(questions[index])
-
-  
-}
-
-function showQuestion(questionObject){
-    questionEl.innerText = questionObject.question
-    answersEl.children
-    for (var i = 0; i < answersEl.children.length; i++) {
-        var answer = answersEl.children[i]
-        var answerObject = questionObject.answers[i]
-        answer.innerText = answerObject.text
-        answer.setAttribute("data-value", answerObject.correct)
-        answer.addEventListener('click', answerSelect)
-    }
-    }
-
-function answerSelect(e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.value
-    console.log(typeof(correct))
-    if (correct === "true"){
-        selectedButton.classList.add("btn.correct")
-        currentQuestion++
-        nextQuestion(currentQuestion)
-        score = score + 10
-    }  
-    if (correct === "false") {
-        selectedButton.classList.add("btn.wrong")
-        secondsLeft = secondsLeft - 10
-        showTime()
-    }
-    if (currentQuestion === questions.length){
-        endGame()
-    }
-
-}
-
-function endGame(){
-    questionContainer.classList.add('hide')
-    highScore.classList.remove('hide')
-    timeEl.classList.add('hide')
-}
-
-
-function inputInitials(){
-}
-backButton.addEventListener('click', function(e){
-    location.reload();
-}, false);
+var clearScoreButton = document.getElementById('clearScore-btn');
+var submitButton = document.getElementById('submit-btn');
+var initialsEl = document.getElementById('initials');
+var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+var max_high_scores = 5;
+var highScoresList = document.getElementById('highScoresList');
 var questions = [
     {
         question: 'The condition in an if else statement is enclosed by',
@@ -138,3 +67,111 @@ var questions = [
 
     },
 ]
+
+
+submitButton.addEventListener('click', submitScore)
+
+startButton.addEventListener('click', start)
+
+function setTime(){
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "Time Left: " + secondsLeft + " s ";
+
+        if(secondsLeft <= 0) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+
+    }, 1000);
+}
+
+
+function start() {
+    startButton.classList.add('hide')
+    questionContainer.classList.remove('hide')
+    setTime()
+    nextQuestion(currentQuestion)
+}
+
+function nextQuestion(index) {
+    if (index <0 || index >= questions.length){
+        endGame()
+        return
+    }
+    showQuestion(questions[index])
+}
+
+function showQuestion(questionObject){
+    questionEl.innerText = questionObject.question
+    answersEl.children
+    for (var i = 0; i < answersEl.children.length; i++) {
+        var answer = answersEl.children[i]
+        var answerObject = questionObject.answers[i]
+        answer.innerText = answerObject.text
+        answer.setAttribute("data-value", answerObject.correct)
+        answer.addEventListener('click', answerSelect)
+    }
+    }
+
+function answerSelect(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.value
+    if (correct === "true"){
+        selectedButton.classList.add("btn.correct")
+        currentQuestion++
+        nextQuestion(currentQuestion)
+        score = score + 10
+
+    }  
+    if (correct === "false") {
+        selectedButton.classList.add("btn.wrong")
+        secondsLeft = secondsLeft - 10
+        currentQuestion++
+        nextQuestion(currentQuestion)
+
+    }
+    if (currentQuestion === questions.length){
+        endGame()
+    }
+
+}
+
+function endGame(){
+    questionContainer.classList.add('hide')
+    highScore.classList.remove('hide')
+    timeEl.classList.add('hide')
+    cardEl.classList.add('hide')
+    finalScore = score + secondsLeft;
+
+}
+
+
+
+function submitScore(){
+   const score = {
+        score: finalScore,
+        name: initialsEl.value
+   };
+   highScores.push(score);
+   highScores.sort((a,b) => b.score - a.score)
+   highScores.splice(5);
+
+   localStorage.setItem('highScores', JSON.stringify(highScores));
+   highScoresList.innerHTML = highScores.map( score => {
+    return `<li class="high-score">${score.name}-${score.score}</li>`;
+    })
+    .join("");
+
+}
+clearScoreButton.addEventListener('click', clearScore);
+
+function clearScore(){
+    localStorage.clear()
+    highScoresList.innerHTML = ''
+}
+
+backButton.addEventListener('click', function(e){
+    location.reload();
+}, false);
+
